bbb12738b8ae831692477a17cf3980160281ff2c
viscousmink <mitchell.klingler99@gmail.com>
2018-12-26 16:41:49 -0500

    Update instruction.java

--- instruction.java
+++ instruction.java
@@ -1,6 +1,9 @@
+import java.awt.*;
+import java.lang.*;
+
 public class instuction {
 
   public static void main(String[] args) {
-    
+    System.out.println("Starting the CHIP-8 interpreter");
   }
 }
